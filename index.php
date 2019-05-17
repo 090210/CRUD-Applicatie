03c64c0c188ec6d89b40cacb45cd139a81a67564
Reda <49568544+090210@users.noreply.github.com>
2019-05-17 10:43:37 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 ?>
 <html>
 <head>
-    <title>php8_opdr01b | <?= basename(__FILE__, '.php'); ?></title>
+    <title>php8_opdr01a | <?= basename(__FILE__, '.php'); ?></title>
     </title>
 </head>
 <body>
