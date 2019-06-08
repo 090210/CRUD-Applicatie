aa26c97c281dfa4d8eb3776ef8f6d69f1b415b70
Reda <1034840@mborijnland.nl>
2019-06-08 09:25:19 +0200

    updated navigation bar

--- delete.php
+++ delete.php
@@ -3,6 +3,7 @@
         <li><a href="index.php">Home</a></li>
         <li><a href="about.php">About</a></li>
         <li><a href="contact.php">Contact</a></li>
+        <li><a href="read.php">Read Data</a></li>
     </ul>
 </nav>
 <?php
