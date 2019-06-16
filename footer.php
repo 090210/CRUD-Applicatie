9b1ad8ac71efb84701be4c7ca50125c5ab24ec4a
Reda <1034840@mborijnland.nl>
2019-06-16 11:51:40 +0200

    Created footer

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="footer mt-auto py-3">
+  <div class="footer-copyright text-center py-3">© <?= date("Y"); ?> Copyright:
+    <a href="https://github.com/090210">Reda</a>
+  </div>
+</footer>
+</html>
