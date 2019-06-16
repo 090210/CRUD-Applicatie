d4d2d99793bc8a090813888da5dc3656fb87796d
Reda <1034840@mborijnland.nl>
2019-06-16 10:43:36 +0200

    added a css line

--- header.php
+++ header.php
@@ -8,4 +8,9 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/e8bfc111b4.js"></script>
     <title><?= $title ?></title>
+    <style>
+        * {
+            word-break: break-all;
+        }
+    </style>
 </head>
